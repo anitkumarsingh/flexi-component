a692d1427956a5c253e228aa329c0592703d64f9
Anitkumarsingh <rockymaster118@gmail.com>
2018-11-05 12:34:01 +0530

    correction

--- src/App.js
+++ src/App.js
@@ -13,11 +13,10 @@ class App extends Component {
     }
   }
   onFlexiSubmit = (Data) =>{
-    let id = +new Date();
-    console.log(this.state.datas);
+    // console.log(this.state.datas);
     // alert(JSON.stringify(Data));
     this.setState({
-      datas:[id+Data,...this.state.datas] 
+      datas:[Data,...this.state.datas] 
       // appending data reveived from child component with parent state's data
     })
   }
